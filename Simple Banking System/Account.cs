9268f6eb497233c611a74b1fe14dc073b56e9892
TPNYC\azhu <azhu@transperfect.com>
2023-06-08 10:04:39 -0400

    working simple banking app

---
+++ Simple Banking System/Account.cs
@@ -0,0 +1,53 @@
+﻿interface IAccount
+{
+    public int Number
+    { get; set; }
+    public string Name
+    { get; set; }
+    public decimal Balance
+    { get; set; }
+    public decimal withdraw(decimal amount);
+    public decimal deposit(decimal amount);
+}
+
+class Account : IAccount
+{
+    private int _number;
+    private string _name;
+    private decimal _balance;
+
+    public Account(string name)
+    {
+        Number = Bank.Instance.getAccountsLength() + 1;
+        Name = name;
+        Balance = 0;
+    }
+
+
+    public int Number
+    {
+        get { return _number; }
+        set { _number = value; }
+    }
+    public string Name
+    {
+        get { return _name; }
+        set { _name = value; }
+    }
+    public decimal Balance
+    {
+        get { return _balance; }
+        set { _balance = value; }
+    }
+    public decimal withdraw(decimal amount)
+    {
+        if (amount > Balance) throw new Exception("Balance is less than withdrawal amount.");
+        Balance -= amount;
+        return Balance;
+    }
+    public decimal deposit(decimal amount)
+    {
+        Balance += amount;
+        return Balance;
+    }
+}
---
+++ Simple Banking System/Bank.cs
@@ -0,0 +1,57 @@
+﻿interface IBank
+{
+    public static Bank? Instance { get; }
+    public void deposit(int id, decimal amount);
+    public void withdraw(int id, decimal amount);
+    public decimal balance(int id);
+    public bool accountExists(int id);
+    public int getAccountsLength();
+    public int createAccount();
+}
+
+class Bank: IBank
+{
+    private static Bank? _instance;
+    private static readonly object padlock = new();
+
+    protected Bank() { }
+    public static Bank Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (_instance == null) _instance = new Bank();
+                return _instance;
+            }
+        }
+    }
+
+    private Dictionary<int, Account> _accounts = new Dictionary<int, Account> { };
+
+    private Account getAccount(int id) => _accounts[id];
+    void addAccount(Account account) => _accounts[account.Number] = account;
+    public void deposit(int id, decimal amount) => getAccount(id).deposit(amount);
+    public void withdraw(int id, decimal amount) => getAccount(id).withdraw(amount);
+    public decimal balance(int id) => getAccount(id).Balance;
+    public bool accountExists(int id) => _accounts.ContainsKey(id);
+    public int getAccountsLength() => _accounts.Count;
+    public int createAccount()
+    {
+        Console.WriteLine("Enter your name:");
+        string name = Console.ReadLine() ?? "";
+        while (name.Length == 0)
+        {
+            Console.WriteLine("Name length must be greater than 0, please re-enter.");
+            name = Console.ReadLine() ?? "";
+        }
+        while (name.Any(c => char.IsDigit(c)))
+        {
+            Console.WriteLine("Please enter only characters for name (no numbers).");
+            name = Console.ReadLine() ?? "";
+        }
+        var account = new Account(name);
+        addAccount(account);
+        return account.Number;
+    }
+}
